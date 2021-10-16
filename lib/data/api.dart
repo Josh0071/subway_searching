41d484b63cc22619415fe433ab57152678ea2521
Josh0071 <jsooha00@gmail.com>
2021-10-16 22:37:35 +0900

    api 수정

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:image_searching_app/model/real_time_arrival_list.dart';
 
@@ -5,7 +6,11 @@ class SubwayApi {
   static const baseUrl = 'http://swopenapi.seoul.go.kr/api';
 
   Future<List<RealtimeArrivalList>?>? getImages(String station) async {
-    await http.get(Uri.parse(
+    final result = await http.get(Uri.parse(
         '$baseUrl/subway/sample/json/realtimeStationArrival/0/5/$station'));
+
+    final jsonResponse = jsonDecode(result.body);
+    Iterable itemsJson = jsonResponse['realtimeArrivalList'];
+    return itemsJson.map((e) => RealtimeArrivalList.fromJson(e)).toList();
   }
 }
--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -33,106 +33,108 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(
         title: const Text('지하철 실시간 정보'),
       ),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 15,
-              ),
-              Expanded(
-                child: Form(
-                  key: _formkey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        controller: _controller,
-                        validator: (value) {
-                          if (value!.trim().isEmpty) {
-                            return '올바른 검색어를 입력하세요.';
-                          }
-                          return null;
-                        },
-                        decoration: const InputDecoration(
-                          labelText: '역 이름음 검색 해주세요',
-                          border: OutlineInputBorder(),
+      body: SafeArea (maintainBottomViewPadding: true,
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Form(
+                    key: _formkey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          controller: _controller,
+                          validator: (value) {
+                            if (value!.trim().isEmpty) {
+                              return '올바른 검색어를 입력하세요.';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                            labelText: '역 이름음 검색 해주세요',
+                            border: OutlineInputBorder(),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  if (_formkey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(const SnackBar(content: Text('error')));
-                  }
-                  setState(() {});
-                  FocusScope.of(context).unfocus();
-                },
-                child: const Text('조회'),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          const Center(
-            child: Text(
-              '도착 정보',
-              style: TextStyle(
-                fontSize: 30,
+                TextButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(const SnackBar(content: Text('error')));
+                    }
+                    setState(() {});
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: const Text('조회'),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            const Center(
+              child: Text(
+                '도착 정보',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
             ),
-          ),
-          FutureBuilder<List<RealtimeArrivalList>?>(
-            future: _api
-                .getImages(_controller.text.isEmpty ? '서울' : _controller.text),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                Column(
-                  children: const [
-                    SizedBox(
-                      height: 250,
-                    ),
-                    Center(
-                        child: Align(
-                            alignment: Alignment.center,
-                            child: CircularProgressIndicator())),
-                  ],
-                );
-              }
-              if (!snapshot.hasData) {
-                return const Center(child: Text('역 이름을 올바르게 입력하세요.'));
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Column(
-                  children: const [
-                    SizedBox(
-                      height: 250,
-                    ),
-                    Center(
-                        child: Align(
-                            alignment: Alignment.center,
-                            child: CircularProgressIndicator())),
-                  ],
-                );
-              }
+            FutureBuilder<List<RealtimeArrivalList>?>(
+              future: _api
+                  .getImages(_controller.text.isEmpty ? '서울' : _controller.text),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  Column(
+                    children: const [
+                      SizedBox(
+                        height: 250,
+                      ),
+                      Center(
+                          child: Align(
+                              alignment: Alignment.center,
+                              child: CircularProgressIndicator())),
+                    ],
+                  );
+                }
+                if (!snapshot.hasData) {
+                  return const Center(child: Text('역 이름을 올바르게 입력하세요.'));
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Column(
+                    children: const [
+                      SizedBox(
+                        height: 250,
+                      ),
+                      Center(
+                          child: Align(
+                              alignment: Alignment.center,
+                              child: CircularProgressIndicator())),
+                    ],
+                  );
+                }
 
-              final result = snapshot.data!;
-              return ListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: result.map((e) => ImageItem(data: e)).toList(),
-              );
-            },
-          ),
-        ],
+                final result = snapshot.data!;
+                return ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: result.map((e) => ImageItem(data: e)).toList(),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
