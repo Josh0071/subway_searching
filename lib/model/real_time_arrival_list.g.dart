65c58c23909aebe3d532a2d1bc299773d935a8f0
Josh0071 <jsooha00@gmail.com>
2021-10-16 22:03:46 +0900

    json serializable 적용 완료

---
+++ lib/model/real_time_arrival_list.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'real_time_arrival_list.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RealtimeArrivalList _$RealtimeArrivalListFromJson(Map<String, dynamic> json) =>
+    RealtimeArrivalList(
+      json['trainLineNm'] as String,
+      json['arvlMsg2'] as String,
+    );
+
+Map<String, dynamic> _$RealtimeArrivalListToJson(
+        RealtimeArrivalList instance) =>
+    <String, dynamic>{
+      'trainLineNm': instance.trainLineNm,
+      'arvlMsg2': instance.arvlMsg2,
+    };
